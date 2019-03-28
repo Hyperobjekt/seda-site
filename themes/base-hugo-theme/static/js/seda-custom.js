bd4a74f97b01c73d937f077b6998546891975049
A Groshek <amy@amygroshek.com>
2019-03-28 16:01:13 -0500

    Split up states and scroll logic, small fixup for scroll tracking on home page

--- themes/base-hugo-theme/static/js/seda-custom.js
+++ themes/base-hugo-theme/static/js/seda-custom.js
@@ -25,6 +25,18 @@
         $target.parents('.research-paper').toggleClass('abstract-visible');
     }
 
+    function initHomepageAnimations() {
+      $(window).scroll(function() { // Says this function is preformed continuously while scrolling.
+          var windowHeight = $( window ).height(),
+              Scroll = $(window).scrollTop() + windowHeight - 300, // This variable finds the distance you have scrolled from the top.
+              trendGraphic = $('#trendgraphic').offset().top; // This variable finds the distance between #section-one and the top. Replace #section-one with the ID of your section.
+
+          if (Scroll >= trendGraphic) { // If you have scrolled past section one do this.
+              $("#trendgraphic").addClass("move"); // Adds class of current-menu-item to the menu item with a class of menu-item-1
+          }
+      });
+    }
+
     var updateModal = {
         activeBio: null,
         update: function() {
@@ -158,23 +170,25 @@
             $(this).addClass("highlight");
         });
 
-
+        if ($("#trendgraphic").length >= 1) {
+          initHomepageAnimations();
+        }
 
     });
     /* Homepage animations */
-    $( document ).ready(function() { // Tells the function to wait to preform until everything on the page has loaded.
-        $(window).scroll(function() { // Says this function is preformed continuously while scrolling.
-            var windowHeight = $( window ).height(),
-                Scroll = $(window).scrollTop() + windowHeight - 300, // This variable finds the distance you have scrolled from the top.
-                trendGraphic = $('#trendgraphic').offset().top; // This variable finds the distance between #section-one and the top. Replace #section-one with the ID of your section.
-    
-            if (Scroll >= trendGraphic) { // If you have scrolled past section one do this.
-                $("#trendgraphic").addClass("move"); // Adds class of current-menu-item to the menu item with a class of menu-item-1
-            } 
-    
-        });
-    });
+    // $( document ).ready(function() { // Tells the function to wait to preform until everything on the page has loaded.
+    //
+    //
+    //     $(window).scroll(function() { // Says this function is preformed continuously while scrolling.
+    //         var windowHeight = $( window ).height(),
+    //             Scroll = $(window).scrollTop() + windowHeight - 300, // This variable finds the distance you have scrolled from the top.
+    //             trendGraphic = $('#trendgraphic').offset().top; // This variable finds the distance between #section-one and the top. Replace #section-one with the ID of your section.
+    //
+    //         if (Scroll >= trendGraphic) { // If you have scrolled past section one do this.
+    //             $("#trendgraphic").addClass("move"); // Adds class of current-menu-item to the menu item with a class of menu-item-1
+    //         }
+    //     });
+    // });
 
 
 })(jQuery);
-

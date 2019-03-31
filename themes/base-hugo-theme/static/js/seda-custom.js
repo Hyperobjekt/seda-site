764ed6985492758e64c4279cb2fe57147fd44ebc
James Minton <james-minton@github.com>
2019-03-31 12:26:38 -0400

    Update seda-custom.js

--- themes/base-hugo-theme/static/js/seda-custom.js
+++ themes/base-hugo-theme/static/js/seda-custom.js
@@ -29,11 +29,14 @@
       $(window).scroll(function() { // Says this function is preformed continuously while scrolling.
           var windowHeight = $( window ).height(),
               Scroll = $(window).scrollTop() + windowHeight - 300, // This variable finds the distance you have scrolled from the top.
-              hpGraphic = $('.hpgraphic').offset().top; // This variable finds the distance between #section-one and the top. Replace #section-one with the ID of your section.
-
-          if (Scroll >= hpGraphic) { // If you have scrolled past section one do this.
-              $(".hpgraphic").addClass("move"); // Adds class of current-menu-item to the menu item with a class of menu-item-1
+              avgGraphic = $('#avg-graphic').offset().top; 
+              trendGraphic = $('#trendgraphic').offset().top; 
+          if (Scroll >= avgGraphic) { // If you have scrolled past this section do this.
+              $("#avg-graphic").addClass("move"); // Adds class of current-menu-item to the menu item with a class of menu-item-1
           }
+          if (Scroll >= trendGraphic) { // If you have scrolled past this section do this.
+            $("#trendgraphic").addClass("move"); // Adds class of current-menu-item to the menu item with a class of menu-item-1
+        }
       });
     }
 

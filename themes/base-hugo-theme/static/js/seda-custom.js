27ae37c7bbc7a0b5f448824b0c3d412e091196b6
James Minton <james-minton@github.com>
2019-04-02 19:31:39 -0400

    Animate growth graphic

--- themes/base-hugo-theme/static/js/seda-custom.js
+++ themes/base-hugo-theme/static/js/seda-custom.js
@@ -30,10 +30,14 @@
           var windowHeight = $( window ).height(),
               Scroll = $(window).scrollTop() + windowHeight - 300, // This variable finds the distance you have scrolled from the top.
               avgGraphic = $('#avg-graphic').offset().top; 
+              growthGraphic = $('#growth-graphic').offset().top;
               trendGraphic = $('#trendgraphic').offset().top; 
            if (Scroll >= avgGraphic) { // If you have scrolled past this section do this.
               $("#avg-graphic").addClass("move"); // Adds class of current-menu-item to the menu item with a class of menu-item-1
           }  
+          if (Scroll >= growthGraphic) { // If you have scrolled past this section do this.
+            $("#growth-graphic").addClass("move"); // Adds class of current-menu-item to the menu item with a class of menu-item-1
+        }  
            if (Scroll >= trendGraphic) { // If you have scrolled past this section do this.
             $("#trendgraphic").addClass("move");  
         }

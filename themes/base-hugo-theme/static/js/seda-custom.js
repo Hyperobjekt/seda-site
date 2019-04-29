b7494ea3509e07641ea436d7c40ea14c840f226c
James Minton <james-minton@github.com>
2019-04-29 15:29:19 -0400

    Update seda-custom.js

--- themes/base-hugo-theme/static/js/seda-custom.js
+++ themes/base-hugo-theme/static/js/seda-custom.js
@@ -62,7 +62,7 @@
       translateY: [400, 0],
       easing: 'easeInOutSine',
       duration: 1400,
-      direction: normal,
+
      
     });
     anime({
@@ -70,7 +70,6 @@
       translateX: [-500, 0],
       easing: 'easeInOutSine',
       duration: 1400,
-      direction: normal,
      
     });
     

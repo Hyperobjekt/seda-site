a393f719d0b33a0d94a3e0fa71eaa4fe215799d5
Peter Lollo <peteralollo@gmail.com>
2019-11-08 17:52:29 -0800

    Add total count of LMS referrals

--- themes/base-hugo-theme/static/js/src/media-table.js
+++ themes/base-hugo-theme/static/js/src/media-table.js
@@ -12,16 +12,18 @@ if (!String.prototype.startsWith) {
 /**
  * Google Sheets-backed media table
  */
-function createMediaTable(oneLastUpdated, oneRowCount, lastUpdated, rows) {
+function createMediaTable(oneLastUpdated, oneRowCount, lastUpdated, rows, totalLMSReferrals) {
   var updatedOne = Date.parse(oneLastUpdated.slice(0, 10));
   var updatedTwo = Date.parse(lastUpdated.slice(0, 10));
   var dateNum = updatedOne >= updatedTwo ? updatedOne : updatedTwo;
   var updatedDate = new Date(dateNum);
   var updatedStr = updatedDate.toLocaleDateString("en-US");
+  var lmsReferralStr = totalLMSReferrals > 0 ? ', ' + totalLMSReferrals + ' LMS referrals' : ''
 
   $("#lastUpdated").html(updatedStr);
   $("#numLocalStories").html(oneRowCount.toLocaleString());
   $("#numNationalStories").html(rows.length.toLocaleString());
+  $("#numLMSReferrals").html(lmsReferralStr);
 
   $("#national-media-table").append("<tbody></tbody>");
   var tbody = $("#national-media-table tbody");
@@ -56,6 +58,8 @@ $(document).ready(function () {
   var SHEET_END = '/public/values?alt=json';
   var SHEET_1_URL = SHEET_BASE + '1' + SHEET_END;
   var SHEET_2_URL = SHEET_BASE + '2' + SHEET_END;
+  var SHEET_LMS_URL = SHEET_BASE + '3' + SHEET_END;
+
   if ($("#national-media-table").length) {
     $.getJSON(SHEET_1_URL, function (data) {
       var sheetOneUpdated = data.feed.updated.$t;
@@ -67,17 +71,22 @@ $(document).ready(function () {
         var properties = Object.keys(rows[0])
           .filter(function (p) { return p.startsWith("gsx$"); })
           .map(function (p) { return p.substr(4); });
+        var cleanedRows = rows.map(function (r) {
+            var row = {};
+            properties.forEach(function (p) {
+              row[p] = r["gsx$" + p].$t === "" ? null : r["gsx$" + p].$t;
+              if (row[p] === null) {
+                row[p] = '';
+              }
+            });
+            return row;
+          })
 
-        createMediaTable(sheetOneUpdated, sheetOneRowCount, lastUpdated, rows.map(function (r) {
-          var row = {};
-          properties.forEach(function (p) {
-            row[p] = r["gsx$" + p].$t === "" ? null : r["gsx$" + p].$t;
-            if (row[p] === null) {
-              row[p] = '';
-            }
-          });
-          return row;
-        }));
+        $.getJSON(SHEET_LMS_URL, function (data) {
+          var totalLMSReferrals = data.feed.entry.length
+
+          createMediaTable(sheetOneUpdated, sheetOneRowCount, lastUpdated, cleanedRows, totalLMSReferrals);
+        });
       });
     });
   }
